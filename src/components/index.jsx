efc6a4ed58f3449b1f6db29260299b1a45acdad2
maulanas <maulana@troopergears.com>
2021-09-02 00:44:24 +0700

    Developed todo app ver 1.0

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import HomeModal from "./home_modal"
+import RenderTabel03 from "./render_tabel_03"
+import DeleteModal from "./delete_modal"
+import EditModal from "./edit_modal"
+
+export {HomeModal, RenderTabel03, DeleteModal, EditModal}
