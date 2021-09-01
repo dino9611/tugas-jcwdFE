7039b69a9f467b2b6c1eb5a41b04afde37319e8d
maulanas <maulana@troopergears.com>
2021-09-02 00:49:53 +0700

    Ganti nama file component yg berfungsi render data dlm bentuk card

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 import HomeModal from "./home_modal"
-import RenderTabel03 from "./render_tabel_03"
+import RenderTabel03 from "./render_cards"
 import DeleteModal from "./delete_modal"
 import EditModal from "./edit_modal"
 
